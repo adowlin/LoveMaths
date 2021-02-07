be2eddd76e96a2a5ec7114075b1d97282d9ec6e3
Alison Dowling <dowlingalison@gmail.com>
2021-02-07 19:16:21 +0000

    add JS to calculateCorrectAnswer function

--- script.js
+++ script.js
@@ -39,7 +39,18 @@ function checkAnswer() {
 }
 
 function calculateCorrectAnswer() {
+    // Gets the operands (the numbers) and the operator directly from the DOM
 
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}, aborting!`;
+    }
 }
 
 function incrementScore() {
